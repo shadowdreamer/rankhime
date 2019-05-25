d5137bd7e6d3d41d5da64c82b8ebce3092fef24e
JING JIAN <uzumaki@yeah.net>
2019-05-25 16:58:12 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,5 @@
 module.exports = {
     // 选项...
     productionSourceMap:false,
-
-    css:undefined,
-    publicPath: './',
-    outputDir: undefined,
-    assetsDir: undefined,
-    runtimeCompiler: undefined,
-    parallel: undefined
+    publicPath: './'
 }
